f9e83fb8e766a0c68ddfb8703318230b204896d2
Pekka Hautaniemi <pekka.hautaniemi@iki.fi>
2023-01-30 19:34:35 +0200

    Added URI class

---
+++ URI.py
@@ -0,0 +1,18 @@
+class URI:
+    scheme: str
+    path: str
+    params: list
+
+    def __init__(self, scheme: str, path: str, params: list):
+        self.scheme = scheme
+        self.path = path
+        self.params = params
+
+    def getScheme(self) -> str:
+        return self.scheme
+
+    def getPath(self) -> str:
+        return self.path
+
+    def getParams(self) -> list:
+        return self.params
